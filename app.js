b59cf2043ce1b17f278dbfa70a0fdeca0e53a0e7
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-16 17:43:01 +0530

    upd

---
+++ app.js
@@ -0,0 +1,37 @@
+const port = 3000;
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// app.use(express.static(path.join(__dirname, "static")));
+// app.use(express.static('public'))
+// app.use(express.static('files'))
+//THE DEFAULT FORMAT IS ==>> express.static(*root_dir/--serves-all-content, [*options/sub_dir/--serves-specified_dir-only]);
+
+app.use(express.static(path.join(__dirname, "/src/"))); //THIS ALLOWS TO SEND THE FULL DIRECTORY SO ALL FILES CAN LOAD WITHOUT ERROR  //SERVING STATIC FILES
+// app.use(express.static("public"));
+
+
+//ENDPOINTS
+app.get('/', (req,res)=>{
+    // res.sendFile((__dirname));
+
+
+    // res.sendFile(path.join(__dirname,'index.html'));  //CAN ALSO USE THIS
+    res.sendFile(path.join(__dirname,"/src/index.html"));
+});
+
+app.get('/preset', (req,res)=>{
+    // res.sendFile('signup.html');
+    res.sendFile(path.join(__dirname,'/src/The-Presentation.html'));
+});
+
+app.get('/about', (req,res)=>{
+     // res.sendFile('signup.html');
+     res.sendFile(path.join(__dirname,'/src/About.html'));
+ });
+
+app.listen(port, ()=>{
+    console.log(`the server is listening at port ${port}`);
+
+});
